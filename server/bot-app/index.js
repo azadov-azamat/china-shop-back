f8b6f88ce3cb38c9e9bccd93f0543092b16cb78f
azadov-azamat <azamatazadov112@gmail.com>
2024-12-23 19:17:49 +0500

    changes

--- server/bot-app/index.js
+++ server/bot-app/index.js
@@ -2,89 +2,87 @@ require('dotenv').config({});
 
 const { Telegraf, Scenes, Markup, session } = require('telegraf');
 
-// const Redis = require('./services/telegraf-redis-session');
-// const { translate } = require('./utils/translate');
-// const { auth } = require('./middleware/auth');
-// const { setCommands } = require('./utils/commands');
-// const { keyboards, LANGUAGE } = require('./utils/keyboards');
-// const { User } = require('../../db/models');
-// const { trackButton } = require('./utils/general');
-// const destinationWizard = require('./scenes/get-destination');
+const Redis = require('./services/telegraf-redis-session');
+const { translate } = require('./utils/translate');
+const { auth } = require('./middleware/auth');
+const { setCommands } = require('./utils/commands');
+const { keyboards, LANGUAGE } = require('./utils/keyboards');
+const { User } = require('../../db/models');
+const { trackButton } = require('./utils/general');
+const destinationWizard = require('./scenes/get-destination');
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
-// const store = new Redis();
-// const stage = new Scenes.Stage([destinationWizard]);
+const store = new Redis();
+const stage = new Scenes.Stage([destinationWizard]);
 const debug = require('debug')('bot');
 const error = require('debug')('bot:error');
 
-// bot.use(session({ store }));
-// bot.use(stage.middleware());
-// bot.use(auth);
+bot.use(session({ store }));
+bot.use(stage.middleware());
+bot.use(auth);
 
-// bot.use(async (ctx, next) => {
-//   try {
-//     await next();
-//   } catch (err) {
-//     if (err.code === 403 && err.description.includes('bot was blocked by the user')) {
-//       debug(`User ${ctx.from.id} blocked the bot. Cannot send message.`);
-//       if (ctx.chat?.id) {
-//         try {
-//           await User.destroy({
-//             where: {
-//               telegram_id: ctx.from.id,
-//             },
-//           });
-//         } catch (e) {
-//           error(e);
-//         }
-//       }
-//     } else {
-//       error('An unexpected error occurred:', err);
-//     }
-//   }
-// });
+bot.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    if (err.code === 403 && err.description.includes('bot was blocked by the user')) {
+      debug(`User ${ctx.from.id} blocked the bot. Cannot send message.`);
+      if (ctx.chat?.id) {
+        try {
+          await User.destroy({
+            where: {
+              telegram_id: ctx.from.id,
+            },
+          });
+        } catch (e) {
+          error(e);
+        }
+      }
+    } else {
+      error('An unexpected error occurred:', err);
+    }
+  }
+});
 
 // setCommands(bot);
 
 bot.start(async ctx => {
-  // if (ctx.chat.type !== 'private') {
-  //   try {
-  //     const botMember = await ctx.telegram.getChatMember(ctx.chat.id, ctx.botInfo.id);
-  //     if (botMember.status === 'administrator') {
-  //       console.log('test botMember', botMember.status);
-  //       await ctx.telegram.leaveChat(ctx.chat.id);
-  //       return;
-  //     }
-  //   } catch (err) {
-  //     error('Error checking bot status:', err);
-  //     console.log('Error checking bot status:', err);
-  //     return;
-  //   }
-  // }
+  if (ctx.chat.type !== 'private') {
+    try {
+      const botMember = await ctx.telegram.getChatMember(ctx.chat.id, ctx.botInfo.id);
+      if (botMember.status === 'administrator') {
+        console.log('test botMember', botMember.status);
+        await ctx.telegram.leaveChat(ctx.chat.id);
+        return;
+      }
+    } catch (err) {
+      error('Error checking bot status:', err);
+      console.log('Error checking bot status:', err);
+      return;
+    }
+  }
 
-  await ctx.reply("dsgfsdfgsdf");
+  const args = ctx.startPayload;
 
-  // const args = ctx.startPayload;
-  //
-  // ctx.session.page = 'start';
-  //
-  // if (ctx.user) {
-  //   if (!ctx.user.selectedLang) {
-  //     await ctx.reply(translate('select-language'), keyboards('languageWithoutBack'));
-  //   } else {
-  //     ctx.session.page = 'main';
-  //     await ctx.reply(translate('welcome'), {
-  //       parse_mode: 'Markdown',
-  //       ...keyboards('main', { ctx }),
-  //     });
-  //   }
-  //
-  //   if (/^-\d+/.test(args)) {
-  //     await trackButton(ctx, 'start', args);
-  //   } else {
-  //     await trackButton(ctx, 'start');
-  //   }
-  // }
+  ctx.session.page = 'start';
+
+  if (ctx.user) {
+    if (!ctx.user.selectedLang) {
+      await ctx.reply(translate('select-language'), keyboards('languageWithoutBack'));
+    } else {
+      ctx.session.page = 'main';
+      await ctx.reply(translate('welcome'), {
+        parse_mode: 'Markdown',
+        ...keyboards('main', { ctx }),
+      });
+    }
+
+    if (/^-\d+/.test(args)) {
+      await trackButton(ctx, 'start', args);
+    } else {
+      await trackButton(ctx, 'start');
+    }
+  }
 });
 
 // bot.settings(async ctx => {
@@ -109,22 +107,22 @@ bot.start(async ctx => {
 // require('./stage/bot-action')(bot);
 
 // Global Error Handling
-// bot.catch(async (err, ctx) => {
-//   error(`Error for ${ctx.updateType}`, err);
-//   console.log(`Error for ${ctx.updateType}`, err);
-//
-//   if (err.response && err.response.error_code === 403) {
-//     debug(`Bot was blocked by user ${ctx.chat.id}`);
-//     return;
-//   } else if (err.response && err.response.error_code === 429) {
-//     debug(`Rate limit exceeded. Try again`);
-//     return;
-//   } else if (err.code) {
-//     debug(`A generic error occurred: ${err.code}`);
-//   }
-//
-//   ctx.reply('Что-то пошло не так. Пожалуйста, попробуйте позже.');
-// });
+bot.catch(async (err, ctx) => {
+  error(`Error for ${ctx.updateType}`, err);
+  console.log(`Error for ${ctx.updateType}`, err);
+
+  if (err.response && err.response.error_code === 403) {
+    debug(`Bot was blocked by user ${ctx.chat.id}`);
+    return;
+  } else if (err.response && err.response.error_code === 429) {
+    debug(`Rate limit exceeded. Try again`);
+    return;
+  } else if (err.code) {
+    debug(`A generic error occurred: ${err.code}`);
+  }
+
+  ctx.reply('Что-то пошло не так. Пожалуйста, попробуйте позже.');
+});
 
 process.on('unhandledRejection', err => {
   error(err);
